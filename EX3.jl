f1b2999015216918f650a410e9603b5bf4968d65
Lori Cliff <lcliff@princeton.edu>
2019-10-01 19:25:07 -0400

    This is the initial file

---
+++ EX3.jl
@@ -0,0 +1,21 @@
+using JLD
+
+Pkg.add("Images")
+include("standard_start.jl")
+img = imread("el-capitan.png")
+imshow(img)
+println(size(img[:,:,1]))
+println(size(img[:,1,:]))
+println(size(img[1,:,:]))
+
+
+# a function that takes in an image as a filename and returns
+# the three different color breakdowns of that image
+function converToRGB(fileName::String)
+    img = imread(fileName)
+    imshow(img)
+    red = img[:, :, 1]
+    green = img[:, :, 2]
+    blue = img[:, :, 3]
+    return red, green, blue
+end
