a6891e2d8fe3a8deaa24d39d96dc1a792646fd90
Lori Cliff <lcliff@princeton.edu>
2019-10-04 17:38:54 -0400

    Real Final
    Sorry this is the actual final I had to add the comments

--- EX3.jl
+++ EX3.jl
@@ -4,14 +4,30 @@ using JLD
 # include("standard_start.jl")
 img = imread("el-capitan.png")
 imshow(img)
+println("Checking size of the image")
 println(size(img[:,:,1]))
 println(size(img[:,1,:]))
 println(size(img[1,:,:]))
 
 println("Reset with git reset --mixed")
 
-# a function that takes in an image as a filename and returns
-# the three different color breakdowns of that image
+"""
+ConverToRGB -- displays image and returns separated colored peices.
+takes as input a string representing the filename, and
+then loads the image file, displays it, and extracts the
+red, blue, and green channels into separate variables, a
+nd returns the content of those variables
+
+Args:
+   arg1 (str): takes as input a string representing the filename of
+   an image
+
+Returns:
+   out (Float32[360,640, 3]): Returns 3 arrays of color values where
+    the first is the red contents, second is the blue contents, and the
+    third is the green - each of these are [360,640] pixeled arrays
+
+"""
 function converToRGB(fileName::String)
     img = imread(fileName)
     imshow(img)
@@ -24,10 +40,12 @@ end
 # part e
 image2 = zeros(360, 640, 3)
 returnInfo = converToRGB("el-capitan.png")
+# altering the two colors
 image2[:, :, 1] = returnInfo[2]
 image2[:, :, 2] = returnInfo[3]
 image2[:, :, 3] = returnInfo[1]
 
+# plotting the two images next to eachother
 figure(2)
 subplot(1,2,1)
 imshow(img)
@@ -38,8 +56,20 @@ imshow(image2)
 axis("off")
 
 figure(3)
-# function that takes an image name and circular shifts it by the value
-# determined by the integer arguement. It then returns the shifted image.
+"""
+CircularMatrix -- summary line.
+takes an image, circularly moves only one channel, the red channel (the
+first one) up some number of pixels, p, such that the top p rows
+now become the bottom p rows.
+
+Args:
+   arg1 (str): takes as input a string representing the filename of
+   an image
+   arg2 (int): the number of pixels you want to shift it by
+
+Returns:
+   out (Float32[360, 640, 3] (an image)): return the shifted image
+"""
 function circularMatrix(image::String, shiftValue::Int)
     returnThing = converToRGB(image)
     shiftRed = copy(returnThing[1])
@@ -58,9 +88,11 @@ function circularMatrix(image::String, shiftValue::Int)
     return newImage
 end
 
+# calling the circular matrix function
 shiftedImage = circularMatrix("el-capitan.png", 180)
 # imshow(shiftedImage)
 
+# plotting the two images next to each other
 subplot(1,2,1)
 imshow(img)
 title("Orginal compared to image with red shifted")
