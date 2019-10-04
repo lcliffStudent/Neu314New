6418acb747b5cf9ed08f680ff1ca78bfa8bc0be5
Lori Cliff <lcliff@princeton.edu>
2019-10-04 12:35:19 -0400

    Final Assignment
    This is my final submission

--- EX3.jl
+++ EX3.jl
@@ -36,3 +36,35 @@ axis("off")
 subplot(1,2,2)
 imshow(image2)
 axis("off")
+
+figure(3)
+# function that takes an image name and circular shifts it by the value
+# determined by the integer arguement. It then returns the shifted image.
+function circularMatrix(image::String, shiftValue::Int)
+    returnThing = converToRGB(image)
+    shiftRed = copy(returnThing[1])
+    originalRed = copy(returnThing[1])
+    for i = 1:(size(originalRed, 1)-shiftValue)
+        shiftRed[i, :] = originalRed[i+shiftValue, :]
+    end
+    startHere = size(originalRed, 1)-shiftValue
+    for i = 1:shiftValue
+        shiftRed[i + startHere, :] = originalRed[i, :]
+    end
+    newImage = zeros(360, 640, 3)
+    newImage[:, :, 1] = shiftRed
+    newImage[:, :, 2] = returnThing[2]
+    newImage[:, :, 3] = returnThing[3]
+    return newImage
+end
+
+shiftedImage = circularMatrix("el-capitan.png", 180)
+# imshow(shiftedImage)
+
+subplot(1,2,1)
+imshow(img)
+title("Orginal compared to image with red shifted")
+axis("off")
+subplot(1,2,2)
+imshow(shiftedImage)
+axis("off")
